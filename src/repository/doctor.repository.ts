55705a92ae88a10f33d7362a8004e59a3714bd1e
UdayaKiran442 <gudaya2002@gmail.com>
2025-11-08 18:00:16 +0530

    get appointment details

--- src/repository/doctor.repository.ts
+++ src/repository/doctor.repository.ts
@@ -3,7 +3,8 @@ import { AddDoctorInDBError, GetLatestAppointmentForDoctorFromDBError } from "..
 import type { IAssignDoctorSchema } from "../routes/v1/admin.route";
 import { generateNanoId } from "../utils/nanoId.utils";
 import db from "./db";
-import { appointments, doctor } from "./schema";
+import { appointments, doctor, patient } from "./schema";
+import type { IGetDoctorAppointmentSchema } from "../routes/v1/doctors.route";
 
 export async function addDoctorInDB(payload: IAssignDoctorSchema) {
 	try {
@@ -29,7 +30,29 @@ export async function getLatestAppointmentForDoctorFromDB(payload: { doctorId: s
 	try {
 		return await db.select().from(appointments).where(eq(appointments.doctorId, payload.doctorId)).orderBy(desc(appointments.appointmentDate)).limit(1);
 	} catch (error) {
-        console.log("🚀 ~ getLatestAppointmentForDoctorFromDB ~ error:", error)
+		console.log("🚀 ~ getLatestAppointmentForDoctorFromDB ~ error:", error);
+		throw new GetLatestAppointmentForDoctorFromDBError("Failed to get latest appointment for doctor from DB", { cause: (error as Error).message });
+	}
+}
+
+export async function getDoctorAppointmentDetailsFromDB(payload: IGetDoctorAppointmentSchema) {
+	try {
+		const appointmentDetails = await db
+			.select({
+				appointmentId: appointments.appointmentId,
+				doctorId: appointments.doctorId,
+				patientId: appointments.patientId,
+				appointmentDate: appointments.appointmentDate,
+				reasonForVisit: appointments.reasonForVisit,
+				patientName: patient.name,
+				patientEmail: patient.email,
+				patientPhone: patient.phone,
+			})
+			.from(appointments)
+			.where(eq(appointments.appointmentId, payload.appointmentId))
+			.innerJoin(patient, eq(appointments.patientId, patient.patientId));
+		return appointmentDetails[0];
+	} catch (error) {
 		throw new GetLatestAppointmentForDoctorFromDBError("Failed to get latest appointment for doctor from DB", { cause: (error as Error).message });
 	}
 }
--- src/routes/v1/doctors.route.ts
+++ src/routes/v1/doctors.route.ts
@@ -4,9 +4,9 @@ import { authMiddleware } from "../../middleware/auth.middleware";
 import { authorizeMiddleware } from "../../middleware/authorize.middleware";
 import { USER_ROLES } from "../../constants/constants";
 import z from "zod";
-import { getDoctorAppointments } from "../../controller/doctors.controller";
+import { getDoctorAppointmentDetails, getDoctorAppointments } from "../../controller/doctors.controller";
 import { GetDoctorAppointmentsFromDBError } from "../../exceptions/appointments.exceptions";
-import { GetDoctorAppointmentsError } from "../../exceptions/doctor.exceptions";
+import { GetDoctorAppointmentDetailsError, GetDoctorAppointmentDetailsFromDBError, GetDoctorAppointmentsError } from "../../exceptions/doctor.exceptions";
 import logger from "../../services/logger.service";
 
 const doctorsRoute = new Hono();
@@ -38,6 +38,37 @@ doctorsRoute.post("/appointments", authMiddleware, authorizeMiddleware(USER_ROLE
 		}
 		logger.error("Unknown Error: %o", error);
 		return c.json({ success: false, message: "Failed to fetch doctor's appointments", error: (error as Error).message }, 500);
+	}
+});
+
+const GetDoctorAppointmentSchema = z.object({
+	patientId: z.string().describe("ID of the patient"),
+	appointmentId: z.string().describe("ID of the appointment"),
+});
+
+export type IGetDoctorAppointmentSchema = z.infer<typeof GetDoctorAppointmentSchema>;
+
+doctorsRoute.post("/appointment", async (c) => {
+	try {
+		const validation = GetDoctorAppointmentSchema.safeParse(await c.req.json());
+		if (!validation.success) {
+			throw validation.error;
+		}
+		const payload = validation.data;
+		const appointmentDetails = await getDoctorAppointmentDetails(payload);
+		return c.json({ success: true, appointmentDetails }, 200);
+	} catch (error) {
+		if (error instanceof z.ZodError) {
+			const errMessage = JSON.parse(error.message);
+			logger.error("Validation Error: %o", errMessage);
+			return c.json({ success: false, error: errMessage[0], message: errMessage[0].message }, 400);
+		}
+		if (error instanceof GetDoctorAppointmentDetailsError || error instanceof GetDoctorAppointmentDetailsFromDBError) {
+			logger.error("Application Error: %o", error);
+			return c.json({ success: false, message: error.message, error: error.cause }, 500);
+		}
+		logger.error("Unknown Error: %o", error);
+		return c.json({ success: false, message: "Failed to fetch doctor's appointment details", error: (error as Error).message }, 500);
 	}
 });
 
